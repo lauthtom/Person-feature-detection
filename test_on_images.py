20324c0a5b82d093fa154640b109157f2128550a
Tom Lauth <66142861+lauthtom@users.noreply.github.com>
2025-05-23 11:31:55 +0200

    Not yet finished

--- test_on_images.py
+++ test_on_images.py
@@ -1,89 +1,145 @@
 import os
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
+import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-import matplotlib.pyplot as plt
 
-from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import img_to_array, load_img
+from PIL import Image
+from test_on_camera import load_models
+from Face_detection.face_detection import detect_face
 
-model_names = [
-    "Gender_classification.keras",
-    "Beard_classification.keras",
-    "Haircolor_classification.keras",
-    "Nation_classification.keras",
-    "Glasses_classification.keras",
-]
 
-models_dir = "Models/"
-path_to_models = "/Users/tomlauth/Library/CloudStorage/GoogleDrive-santoxhd@gmail.com/Meine Ablage/Studium/Master/2 FS SoSe/Individual Profiling/Old_Models"
+def classify_frame(image: np.ndarray) -> tuple[str, str, str, str, str]:
+    gender_prediction = models["Gender_classification"].predict(
+        image)
+    beard_prediction = models["Beard_classification"].predict(
+        image)
+    haircolor_prediction = models["Haircolor_classification"].predict(
+        image)
+    nation_prediction = models["Nation_classification"].predict(
+        image)
+    glasses_prediction = models["Glasses_classification"].predict(
+        image)
 
-if len(os.listdir(path_to_models)) == len(model_names):
+    gender = "Male" if gender_prediction[0][0] > 0.5 else "Female"
+    beard = "No Beard" if beard_prediction[0][0] > 0.5 else "Beard"
+    haircolor = haircolor_labels[np.argmax(haircolor_prediction)]
+    glasses = "No Glasses" if glasses_prediction[0][0] > 0.5 else "Glasses"
+    nation = nation_labels[np.argmax(nation_prediction)]
 
+    return gender, gender_prediction[0][0], beard, beard_prediction[0][0], haircolor, glasses, glasses_prediction[0][0], nation
+
+
+if __name__ == "__main__":
+    # All the necessary variables
+    haircolor_labels = ["Black hair", "Blond hair", "Brown hair", "Gray hair"]
+    nation_labels = ["Asian", "Black", "Indian", "Others", "White"]
+    path_to_models = "saved_models/"
+    target_size = (224, 224)
     models = {}
+    model_names = [
+        "Gender_classification",
+        "Beard_classification",
+        "Haircolor_classification",
+        "Nation_classification",
+        "Glasses_classification",
+    ]
 
-    for model_name in model_names:
-        model_path = os.path.join(path_to_models, model_name)
-        print(f"Trying to load the {model_name} model...")
-        models[model_name] = tf.keras.models.load_model(model_path)
-        print(f"Successfully loaded the {model_name} model.")
+    # Parallel loading of the different models
+    with ThreadPoolExecutor(max_workers=len(model_names)) as executor:
+        futures = [executor.submit(load_models, name, path_to_models)
+                   for name in model_names]
+        for future in as_completed(futures):
+            name, model = future.result()
+            models[name] = model
 
     print("All models loaded successfully!")
 
     answer = input(
-        "Do you want to continue predicting with live video? (Y/N)").upper()
+        "Do you want to continue predicting with static images? (Y/N)").upper()
 
     if answer == "Y":
-        pass
+        images_dir = os.listdir("Images/")
+        results = []
+
+        for image in images_dir:
+            if image.endswith(".jpg"):
+                image_path = os.path.join("Images/", image)
+                img = load_img(f"Images/{image}", target_size=target_size)
+                img_array = img_to_array(img)
+                img_array = img_array / 255.0
+                img_array = np.expand_dims(img_array, axis=0)
+
+                # gender, gender_accuracy, beard, beard_accuracy, haircolor, glasses, glasses_accuracy, nation = classify_frame(
+                #     img_array)
+
+                img_detected_face = detect_face(
+                    image_path=image_path, padding=0.2)
+
+                # TODO: Das hier ist noch nicht fertig, Bilder muessen irgendwie,
+                # entweder gleichzeitig angezeigt werden, oder nacheinander
+                if img_detected_face is not None:
+                    if isinstance(img_detected_face, np.ndarray):
+                        img_detected_face = Image.fromarray(img_detected_face)
+
+                    img_detected_face = img_detected_face.resize(target_size)
+                    img_array = img_to_array(img_detected_face)
+                    img_array = img_array / 255.0
+                    img_array = np.expand_dims(img_array, axis=0)
+                    gender, gender_accuracy, beard, beard_accuracy, haircolor, glasses, glasses_accuracy, nation = classify_frame(
+                        img_array)
+
+                results.append((img, gender, beard, glasses, haircolor, nation))
+
+                n = len(results)
+                cols = 3
+                rows = (n + cols - 1) // cols
+                fig, axes = plt.subplots(
+                    rows, cols, figsize=(5 * cols, 5 * rows))
+                axes = axes.flatten()
+
+                for idx, (img, gender, beard, glasses, haircolor, nation) in enumerate(results):
+                    axes[idx].imshow(img)
+                    axes[idx].axis("off")
+                    axes[idx].set_title(
+                        f"Gender: {gender}\nBeard: {beard}\nGlasses: {glasses}\nHaircolor: {haircolor}\nNation: {nation}",
+                        fontsize=10, loc='left'
+                    )
+
+                for idx in range(len(results), len(axes)):
+                    axes[idx].axis("off")
+
+                plt.tight_layout()
+                plt.show()
+
+                # FIXME: ALTER CODE!!!
+                # image_width = img.size[0]
+                # image_height = img.size[1]
+
+                # # plt.figure(figsize=(9, 9))
+
+                # dpi = 100
+
+                # figsize = (image_width / dpi, image_height / dpi)
+
+                # fig = plt.figure(figsize=figsize, dpi=dpi)
+                # ax = fig.add_axes([0, 0, 1, 1])
+                # ax.imshow(img)
+                # ax.axis("off")
+
+                # ax.set_title(
+                #     f"Gender: {gender}\nBeard: {beard}\nGlasses: {glasses}\nHaircolor: {haircolor}\nNation: {nation}",
+                #     fontsize=10,
+                #     loc='left'
+                # )
+
+                # print(
+                #     f"IMAGE: {img}\nGender: {gender}, accuracy: {gender_accuracy:.2f}\nBeard: {beard}, accuracy: {beard_accuracy:.2f} glasses: {glasses}, accuracy: {glasses_accuracy:.2f} haircolor: {haircolor}\nNation: {nation}")
+
+                # plt.show()
+            else:
+                print(f"Skipped {image}, because it's not a image")
     else:
         exit(1)
-else:
-    print("There are not all .keras models in the directory. Please check it again!")
-
-images_dir = os.listdir("Images/")
-print(models)
-
-# All labels for each feature with more than 2 values
-haircolor_labels = ["Black hair", "Blond hair", "Brown hair", "Gray hair"]
-nation_labels = ["Asian", "Black", "Indian", "Others", "White"]
-
-target_size = (224, 224)
-
-for image in images_dir:
-
-    if image.endswith(".jpg"):
-        img = load_img(f"Images/{image}", target_size=target_size)
-        img_array = img_to_array(img)
-        img_array = img_array / 255.0
-        img_array = np.expand_dims(img_array, axis=0)
-
-        gender_pred = models["Gender_classification.keras"].predict(img_array)
-        beard_pred = models["Beard_classification.keras"].predict(img_array)
-        haircolor_pred = models["Haircolor_classification.keras"].predict(
-            img_array)
-        nation_pred = models["Nation_classification.keras"].predict(img_array)
-        glasses_pred = models["Glasses_classification.keras"].predict(
-            img_array)
-
-        image_width = img.size[0]
-        image_height = img.size[1]
-
-        plt.figure(figsize=(9, 9))
-
-        plt.imshow(img)
-        plt.axis("off")
-
-        x_pos = image_width / 2
-        y_pos = image_height + 10
-
-        gender = "Male" if gender_pred[0][0] > 0.5 else "Female"
-        beard = "No Beard" if beard_pred[0][0] > 0.5 else "Beard"
-        glasses = "No Glasses" if glasses_pred[0][0] > 0.5 else "Glasses"
-        haircolor = haircolor_labels[np.argmax(haircolor_pred)]
-        nation = nation_labels[np.argmax(nation_pred)]
-
-        plt.title(
-            f"Gender: {gender}\nBeard: {beard}\nGlasses: {glasses}\nHaircolor: {haircolor}\nNation: {nation}"
-        )
-        plt.show()
-    else:
-        print(f"Skipped {image}, because it's not a image")
